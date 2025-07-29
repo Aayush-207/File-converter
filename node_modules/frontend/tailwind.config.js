acc0296c7921df792ab4fe38585c21c4e9c83b41
Aayush-207 <aayushraut2006@gmail.com>
2025-07-29 11:16:10 +0530

    -

---
+++ node_modules/frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f25c54', // more intense red
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}; 
